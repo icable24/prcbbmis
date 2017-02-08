daae8ef50bf59d45c2de6c5df4566c2c76e1b20d
icable24 <icable130@gmail.com>
2017-02-08 18:15:40 +0800

    Dispensing

--- php/confirmRequest.php
+++ php/confirmRequest.php
@@ -8,8 +8,11 @@
 	if($id == '0'){
 		$update = $pdo->prepare("UPDATE bloodrequest SET status = 'Approved' WHERE pid = '$pid'");
 		$update->execute();
+		header("Location: ../viewrequest.php");
 	}else{
 		$update = $pdo->prepare("UPDATE bloodrequest SET status = 'Denied' WHERE pid = '$pid'");
-		$update->execute();	
+		$update->execute();
+		header("Location: ../viewrequest.php");	
 	}
+	header("Location: ../viewrequest.php");
 ?>
